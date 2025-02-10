53673748a01909ac2983cf1111b7f7d29b4efafb
DehydratedFish <25441148+DehydratedFish@users.noreply.github.com>
2025-02-10 14:19:42 +0100

    Adding array slice function.

--- source/array.h
+++ source/array.h
@@ -3,7 +3,22 @@
 #include "definitions.h"
 
 
-template<typename Type>
+
+template<class Type>
+Array<Type> slice(Array<Type> array, s64 pos, s64 size) {
+    assert(pos >= 0 && size >= 0);
+#if BOUNDS_CHECKING
+    if (array.size < pos || array.size < pos + size) die("Slice out of bounds.");
+#endif
+
+    Array<Type> result = {};
+    result.data = array.data + pos;
+    result.size = size;
+
+    return result;
+}
+
+template<class Type>
 void reverse(Array<Type> array) {
     s64 mid = array.size / 2;
     for (s64 i = 0; i < mid; i += 1) {
