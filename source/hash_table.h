22c09f780b0cdb1eb61de6c8b388244a6c618912
DehydratedFish <25441148+DehydratedFish@users.noreply.github.com>
2025-03-11 19:07:58 +0100

    Changed some HashTable template things so it is easier to use.

--- source/hash_table.h
+++ source/hash_table.h
@@ -3,20 +3,31 @@
 #include "memory.h"
 
 
-inline u64 basic_string_hash(String str) {
+inline u64 basic_hash(u32 *value) {
+    return *value;
+}
+
+inline u64 basic_hash(String *str) {
     u64 h = 0x100;
 
-    for (s64 i = 0; i < str.size; i += 1) {
-        h ^= str[i];
+    for (s64 i = 0; i < str->size; i += 1) {
+        h ^= str->data[i];
         h *= 1111111111111111111u;
     }
 
     return h;
 }
 
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
+
+// NOTE: I am too lazy to type out these template things every time.
+#define TEMPLATE_DEFINITION template<class KeyType, class ValueType, class HashType = u64, HashType(*HashFunc)(KeyType*) = basic_hash>
+#define TABLE_TYPE HashTable<KeyType, ValueType, HashType, HashFunc>
+#define ENTRY_TYPE typename TABLE_TYPE::Entry
+
+TEMPLATE_DEFINITION
 struct HashTable {
-    static s64 const FirstValidHash = 1;
+    // TODO: Can hashes be something other than integers?
+    static HashType const FirstValidHash = 1;
 
     // TODO: Put this outside the HashTable struct?
     //       Not sure if the template/namespace hassle is worth it otherwise.
@@ -36,22 +47,9 @@ struct HashTable {
     // TODO: Use u64 for the size instead?
 };
 
-template<class ValueType>
-using StringTable = HashTable<String, ValueType, u64, basic_string_hash>;
 
-
-template<class ValueType>
-struct HashTableFindResult {
-    ValueType *found;
-    s64        index;
-};
-
-
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-void init(HashTable<KeyType, ValueType, HashType, HashFunc> *table, s32 exponent, Allocator alloc = DefaultAllocator) {
-    typedef HashTable<KeyType, ValueType, HashType, HashFunc> Table;
-	typedef typename Table::Entry Entry;
-
+TEMPLATE_DEFINITION
+void init(TABLE_TYPE *table, s32 exponent, Allocator alloc = DefaultAllocator) {
 	r32 const max_load = 0.6f;
 
     // NOTE: Use u64 for the size instead?
@@ -64,15 +62,15 @@ void init(HashTable<KeyType, ValueType, HashType, HashFunc> *table, s32 exponent
 
     s64 initial_size = 1LL << exponent;
 
-	table->entries  = ALLOC(alloc, Entry, initial_size);
+	table->entries  = ALLOC(alloc, ENTRY_TYPE, initial_size);
 	table->exponent = exponent;
     table->used     = 0;
 	table->alloc    = initial_size;
 	table->max_load = (s64)(initial_size * max_load);
 }
 
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-void destroy(HashTable<KeyType, ValueType, HashType, HashFunc> *table) {
+TEMPLATE_DEFINITION
+void destroy(TABLE_TYPE *table) {
     if (table->allocator.allocate) {
         DEALLOC(table->allocator, table->entries, table->alloc);
 
@@ -92,11 +90,9 @@ inline s64 hash_table_lookup(u64 hash, s32 exponent, s64 index) {
 }
 
 
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-void grow(HashTable<KeyType, ValueType, HashType, HashFunc> *table) {
-    typedef HashTable<KeyType, ValueType, HashType, HashFunc> Table;
-
-    Table new_table = {};
+TEMPLATE_DEFINITION
+void grow(TABLE_TYPE *table) {
+    TABLE_TYPE new_table = {};
     init(&new_table, table->exponent + 1, table->allocator);
 
     for (s64 i = 0; i < table->alloc; i += 1) {
@@ -109,63 +105,84 @@ void grow(HashTable<KeyType, ValueType, HashType, HashFunc> *table) {
     *table = new_table;
 }
 
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-HashTableFindResult<ValueType> find(HashTable<KeyType, ValueType, HashType, HashFunc> *table, KeyType key) {
-    typedef HashTable<KeyType, ValueType, HashType, HashFunc> Table;
-	typedef typename Table::Entry Entry;
+TEMPLATE_DEFINITION
+ValueType *find(TABLE_TYPE *table, KeyType key) {
+    if (table->alloc == 0) return 0;
 
-    // NOTE: Can't just return nothing on an empty map, because the resulting index would
-    //       point to unallocated memory.
-    s32 const default_exponent = 6;
-    if (table->alloc == 0) init(table, default_exponent);
-
-    u64 h = HashFunc(key);
+    u64 h = HashFunc(&key);
     if (h < table->FirstValidHash) h = table->FirstValidHash;
     for (s64 i = h;;) {
         i = hash_table_lookup(h, table->exponent, i);
 
-        Entry *entry = &table->entries[i];
+        ENTRY_TYPE *entry = &table->entries[i];
         if (entry->hash == 0) {
-            // TODO: Growing table while finding. Could lead to reallocation
-            //       although you are only interrested in knowing if a key exists.
-            //       But I think this is worth it as it makes inserting easier.
-            if (table->used == table->max_load) {
-                grow(table);
-                return find(table, key);
-            }
-            return {0, i};
+            return 0;
         } else if (entry->key == key) {
-            return {&entry->value, i};
+            return &entry->value;
         }
     }
 }
 
 
+TEMPLATE_DEFINITION
+ValueType *insert(TABLE_TYPE *table, KeyType key, ValueType value) {
+    s32 const default_exponent = 6;
+    if (table->alloc == 0) init(table, default_exponent);
 
-// TODO: KeyType is passed by value. This could be problematic if it is a big struct.
-//       But usually keys are integers or strings. Who would shove a std::iostream in here?
-//       Also for now the slot gets overidden. Should the original value be kept?
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-ValueType *insert(HashTable<KeyType, ValueType, HashType, HashFunc> *table, KeyType key, ValueType value) {
-    auto result = find(table, key);
-    return insert_at(table, result.index, key, value);
+    // NOTE: Technically the table doesn't need to grow if the value is never inserted.
+    //       But if the grow() is happening inside the for loop the hash needs to be
+    //       recalculated and I think the complexity is not worth it.
+    if (table->used + 1 > table->max_load) grow(table);
+
+    u64 h = HashFunc(&key);
+    if (h < table->FirstValidHash) h = table->FirstValidHash;
+    for (s64 i = h;;) {
+        i = hash_table_lookup(h, table->exponent, i);
+
+        ENTRY_TYPE *entry = &table->entries[i];
+        if (entry->hash == 0) {
+            entry->hash  = h;
+            entry->key   = key;
+            entry->value = value;
+
+            table->used += 1;
+
+            return &entry->value;
+        } else if (entry->key == key) {
+            return 0;
+        }
+    }
 }
 
-// NOTE: Never use an index that is not returned from a previous call to find.
-template<class KeyType, class ValueType, class HashType, HashType(*HashFunc)(KeyType key)>
-ValueType *insert_at(HashTable<KeyType, ValueType, HashType, HashFunc> *table, s64 index, KeyType key, ValueType value) {
-    assert(index >= 0 && index < table->alloc);
+TEMPLATE_DEFINITION
+ValueType *upsert(TABLE_TYPE *table, KeyType key) {
+    s32 const default_exponent = 6;
+    if (table->alloc == 0) init(table, default_exponent);
 
-    HashType hash = HashFunc(key);
-    if (hash < table->FirstValidHash) hash = table->FirstValidHash;
+    if (table->used + 1 > table->max_load) grow(table);
 
-    auto *entry = &table->entries[index];
-    entry->hash  = hash;
-    entry->key   = key;
-    entry->value = value;
+    u64 h = HashFunc(&key);
+    if (h < table->FirstValidHash) h = table->FirstValidHash;
+    for (s64 i = h;;) {
+        i = hash_table_lookup(h, table->exponent, i);
 
-    table->used += 1;
+        ENTRY_TYPE *entry = &table->entries[i];
+        if (entry->hash == 0) {
+            entry->hash  = h;
+            entry->key   = key;
+            entry->value = {};
 
-    return &entry->value;
+            table->used += 1;
+
+            return &entry->value;
+        } else if (entry->key == key) {
+            return &entry->value;
+        }
+    }
 }
+
+
+#undef TEMPLATE_DEFINITION
+#undef TABLE_TYPE
+#undef ENTRY_TYPE
 
