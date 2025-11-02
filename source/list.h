5e9ebc8fa243b0ff8b8699d4f4a0b1d07da378b7
DehydratedFish <25441148+DehydratedFish@users.noreply.github.com>
2025-11-02 19:02:01 +0100

    Added create_array(), so you can allocate a fitting array from a dynamic List.

--- source/list.h
+++ source/list.h
@@ -225,3 +225,14 @@ void copy_array(List<Type> *list, Array<Type> array) {
     list->size = array.size;
 }
 
+template<class Type>
+Array<Type> create_array(List<Type> list, Allocator alloc = DefaultAllocator) {
+    Array<Type> result = {};
+    result.data = ALLOC(alloc, Type, list.size);
+    result.size = list.size;
+
+    copy_memory(result.data, list.data, list.size * sizeof(Type));
+
+    return result;
+}
+
